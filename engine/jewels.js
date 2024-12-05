76702a0c5db2b84e5549572c7df7b76319fc8a4b
victor viana <victor.deviana@outlook.com>
2024-12-05 20:26:26 -0300

    test modal

---
+++ engine/jewels.js
@@ -0,0 +1,62 @@
+const jewelsJson = [
+  {
+    "image": "/assets/jewel1.webp",
+    "alt": "Imagem de uma Corrente Dupla em Ouro",
+    "title": "Corrente Dupla em Ouro",
+    "price": "R$ 12.499,00"
+  },
+  {
+    "image": "/assets/jewel2.webp",
+    "alt": "Imagem de um Colar de Ouro e Pérolas",
+    "title": "Colar de Ouro e Pérolas",
+    "price": "R$ 19.899,00"
+  },
+  {
+    "image": "/assets/jewel3.webp",
+    "alt": "Imagem de uma Dupla Medalha de Ouro",
+    "title": "Dupla Medalha de Ouro",
+    "price": "R$ 8.799,00"
+  },
+  {
+    "image": "/assets/jewel4.webp",
+    "alt": "Imagem de uma Corrente de Ouro",
+    "title": "Corrente de Ouro",
+    "price": "R$ 11.349,00"
+  },
+  {
+    "image": "/assets/jewel5.webp",
+    "alt": "Imagem de uma Pulseira de Ouro",
+    "title": "Pulseira de Ouro",
+    "price": "R$ 6.799,00"
+  },
+  {
+    "image": "/assets/jewel6.webp",
+    "alt": "Imagem de uma Corrente ou Colar de Ouro",
+    "title": "Corrente/Colar de Ouro",
+    "price": "R$ 15.299,00"
+  },
+  {
+    "image": "/assets/jewel10.webp",
+    "alt": "Imagem de um Colar Duplo de Ouro e Pérolas",
+    "title": "Colar Duplo de Ouro e Pérolas",
+    "price": "R$ 23.499,00"
+  },
+  {
+    "image": "/assets/jewel8.webp",
+    "alt": "Imagem de uma Pulseira Corrente de Ouro",
+    "title": "Pulseira Corrente de Ouro",
+    "price": "R$ 7.299,00"
+  },
+  {
+    "image": "/assets/jewel9.webp",
+    "alt": "Imagem de um Colar Pingente de Ouro",
+    "title": "Colar Pingente de Ouro",
+    "price": "R$ 10.499,00"
+  },
+  {
+    "image": "/assets/jewel11.webp",
+    "alt": "Imagem de um Colar e Medalha de Ouro",
+    "title": "Colar e Medalha de Ouro",
+    "price": "R$ 14.999,00"
+  }
+];
--- engine/script.js
+++ engine/script.js
@@ -1,6 +1,25 @@
+//menu hamburguer
 const menuToggle = document.querySelector('.menu-toggle');
 const navMenu = document.querySelector('.nav-menu');
 
 menuToggle.addEventListener('click', () => {
   navMenu.classList.toggle('show'); 
 });
+
+
+
+jewelsJson.map((item, index) =>{
+  console.log(item)
+})
+
+
+//dados de cada joia
+let jewItem = ''
+
+jewItem.querySelector('.product-img img').src = item.img
+jewItem.querySelector('.product-price').innerHTML = `R$ ${item.price.toFixed(2)}`
+jewItem.querySelector('.product-title').innerHTML = item.name
+
+
+//modal
+const bagButton = document.querySelector('.icon')
