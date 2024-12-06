d32cf8bda9bedf7a5545c953e878c9e4e894e30e
victor viana <victor.deviana@outlook.com>
2024-12-06 00:15:20 -0300

    feat: scriptCart

--- engine/jewels.js
+++ engine/jewels.js
@@ -3,60 +3,70 @@ const jewelsJson = [
     "image": "/assets/jewel1.webp",
     "alt": "Imagem de uma Corrente Dupla em Ouro",
     "title": "Corrente Dupla em Ouro",
-    "price": "R$ 12.499,00"
+    "price": "12.499,00",
+    "desciption": "Com o brilho irresistível do ouro puro, esta joia é um verdadeiro símbolo de elegância e classe. Seu design sofisticado, aliado à atemporalidade do ouro, cria uma peça que combina com qualquer ocasião especial. Para quem busca exclusividade e um toque de luxo em cada detalhe."
   },
   {
     "image": "/assets/jewel2.webp",
     "alt": "Imagem de um Colar de Ouro e Pérolas",
     "title": "Colar de Ouro e Pérolas",
-    "price": "R$ 19.899,00"
+    "price": "19.899,00",
+     "desciption": "Esta joia em ouro foi feita para quem aprecia a perfeição. O acabamento impecável e a beleza do ouro transformam esta peça em um acessório único, que exala sofisticação e refinamento. Ideal para complementar seu estilo com um toque de luxo e exclusividade."
   },
   {
     "image": "/assets/jewel3.webp",
     "alt": "Imagem de uma Dupla Medalha de Ouro",
     "title": "Dupla Medalha de Ouro",
-    "price": "R$ 8.799,00"
+    "price": "8.799,00",
+     "desciption": "O ouro, em sua forma mais pura, brilha com intensidade nesta joia única. Seu design elegante, aliado à riqueza do material, cria uma peça atemporal e cheia de personalidade. Perfeita para aqueles que não abrem mão de sofisticação e exclusividade."
   },
   {
     "image": "/assets/jewel4.webp",
     "alt": "Imagem de uma Corrente de Ouro",
     "title": "Corrente de Ouro",
-    "price": "R$ 11.349,00"
+    "price": "11.349,00",
+     "desciption": "Em cada detalhe desta joia em ouro, há a combinação perfeita entre o luxo e a simplicidade. O brilho dourado e o design sofisticado fazem desta peça uma escolha essencial para quem busca elegância e classe, seja em um evento especial ou no dia a dia."
   },
   {
     "image": "/assets/jewel5.webp",
     "alt": "Imagem de uma Pulseira de Ouro",
     "title": "Pulseira de Ouro",
-    "price": "R$ 6.799,00"
+    "price": "6.799,00",
+     "desciption": "Esta joia em ouro é a expressão máxima da sofisticação e do bom gosto. Seu design refinado destaca o brilho do ouro puro, tornando-a perfeita para ocasiões marcantes. Para quem deseja um acessório que alie luxo, exclusividade e atemporalidade."
   },
   {
     "image": "/assets/jewel6.webp",
     "alt": "Imagem de uma Corrente ou Colar de Ouro",
     "title": "Corrente/Colar de Ouro",
-    "price": "R$ 15.299,00"
+    "price": "15.299,00",
+    "desciption": "Feita para quem aprecia o brilho e a beleza do ouro, esta joia é o toque final de sofisticação para qualquer ocasião. Seu design clássico, mas com um toque moderno, a torna a escolha ideal para aqueles que buscam exclusividade e elegância."
   },
   {
     "image": "/assets/jewel10.webp",
     "alt": "Imagem de um Colar Duplo de Ouro e Pérolas",
     "title": "Colar Duplo de Ouro e Pérolas",
-    "price": "R$ 23.499,00"
+    "price": "23.499,00",
+     "desciption": "Com a beleza imbatível do ouro puro, esta joia é uma verdadeira obra-prima. Seu design clássico e sofisticado faz dela a peça perfeita para realçar sua presença em qualquer ocasião de alto nível."
   },
   {
     "image": "/assets/jewel8.webp",
     "alt": "Imagem de uma Pulseira Corrente de Ouro",
     "title": "Pulseira Corrente de Ouro",
-    "price": "R$ 7.299,00"
+    "price": "7.299,00",
+    "desciption": "Com a leveza do ouro e o acabamento impecável, esta joia é um verdadeiro ícone de sofisticação. Ideal para quem busca um acessório que exale luxo e atemporalidade, perfeita para eventos que exigem elegância."  
   },
   {
     "image": "/assets/jewel9.webp",
     "alt": "Imagem de um Colar Pingente de Ouro",
     "title": "Colar Pingente de Ouro",
-    "price": "R$ 10.499,00"
+    "price": "10.499,00",
+     "desciption": "Esta joia em ouro é o reflexo de um estilo sofisticado e exclusivo. O brilho natural do ouro e seu design simples, mas impactante, tornam-na uma peça indispensável para quem deseja uma joia de alta classe."
   },
   {
     "image": "/assets/jewel11.webp",
     "alt": "Imagem de um Colar e Medalha de Ouro",
     "title": "Colar e Medalha de Ouro",
-    "price": "R$ 14.999,00"
+    "price": "14.999,00",
+     "desciption": "Joia em ouro puro com um design que ressoa sofisticação. Seu brilho radiante e acabamento impecável tornam-na um símbolo de luxo, ideal para aqueles que buscam uma peça que combine com o seu estilo e status."
   }
 ];
--- engine/script.js
+++ engine/script.js
@@ -1,25 +1,99 @@
 //menu hamburguer
-const menuToggle = document.querySelector('.menu-toggle');
-const navMenu = document.querySelector('.nav-menu');
+const menuToggle = document.querySelector(".menu-toggle");
+const navMenu = document.querySelector(".nav-menu");
 
-menuToggle.addEventListener('click', () => {
-  navMenu.classList.toggle('show'); 
+menuToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("show");
 });
 
+const removeButton = document.getElementsByClassName("removeButton");
+for (let i = 0; i < removeButton.length; i++) {
+  removeButton[i].addEventListener("click", removeItem);
+}
 
+const quantityInputs = document.getElementsByClassName("quantity");
+for (let i = 0; i < quantityInputs.length; i++) {
+  quantityInputs[i].addEventListener("change", updateTotal);
+}
 
-jewelsJson.map((item, index) =>{
-  console.log(item)
-})
+const addToCartButtons = document.getElementsByClassName(
+  "button-hover-background"
+);
+for (let i = 0; i < addToCartButtons.length; i++) {
+  addToCartButtons[i].addEventListener("click", addProductToCart);
+}
 
+function addProductToCart(event) {
+  const button = event.target;
+  const productsInfos = button.parentElement.parentElement;
+  const productImage =
+    productsInfos.getElementsByClassName("product-image")[0].src;
+  const productTitle =
+    productsInfos.getElementsByClassName("product-title")[0].innerText;
+  const productPrice =
+    productsInfos.getElementsByClassName("product-price")[0].innerText;
 
-//dados de cada joia
-let jewItem = ''
+  let newCartProduct = document.createElement("div");
+  newCartProduct.classList.add("bag-item");
+  newCartProduct.innerHTML = `
+  <div class="bag-flex">
+        <img class="product-image" src="${productImage}" alt="Imagem em miniatura de ${productTitle}">
+        <p class="product-title">${productTitle}</p>
+        <p class="product-price">R$ ${productPrice}</p>
+        <input type="number" class="quantity" name="qtde" id="qtde" value="1" min="0" max="99" oninput="validateMax(this)">
+        <button class="removeButton">x</button>
+  </div>`;
 
-jewItem.querySelector('.product-img img').src = item.img
-jewItem.querySelector('.product-price').innerHTML = `R$ ${item.price.toFixed(2)}`
-jewItem.querySelector('.product-title').innerHTML = item.name
+  const asideBagItens = document.querySelector("aside .bag-itens");
+  asideBagItens.append(newCartProduct);
+}
 
+//removeButton
+function removeItem(event) {
+  event.target.parentElement.parentElement.remove();
+  updateTotal();
+}
 
-//modal
-const bagButton = document.querySelector('.icon')
+//modal de bolsa
+const bag = document.querySelector(".bag");
+const overlay = document.getElementById("overlay");
+const openButton = document.getElementById("openBag");
+const closeButton = document.querySelector(".closeButton");
+function openBag() {
+  bag.classList.add("open");
+  overlay.classList.add("show");
+}
+function closeBag() {
+  bag.classList.remove("open");
+  overlay.classList.remove("show");
+}
+openButton.addEventListener("click", openBag);
+overlay.addEventListener("click", closeBag);
+closeButton.addEventListener("click", closeBag);
+
+//valor maximo/minimo input
+function validateMax(input) {
+  if (parseInt(input.value) > 99) {
+    input.value = 99;
+  } else if (parseInt(input.value) < 0) {
+    input.value = 0;
+  }
+}
+
+//atualização no valor
+function updateTotal() {
+  let totalAmount = 0;
+  const bagItens = document.getElementsByClassName("bag-item");
+  for (let i = 0; i < bagItens.length; i++) {
+    const productPrice = bagItens[i]
+      .getElementsByClassName("product-price")[0]
+      .innerText.replace("R$", "")
+      .replace(",", ".");
+    const productQtde = bagItens[i].getElementsByClassName("quantity")[0].value;
+
+    totalAmount += productPrice * productQtde;
+  }
+  document.querySelector(
+    ".product-price.final"
+  ).innerText = `R$ ${totalAmount.toFixed(2)}`;
+}
